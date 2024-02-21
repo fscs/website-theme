cacb2bc48e60fb288d677175ec29d6fcad5d90a0
0x5a4 <54070204+0x5a4@users.noreply.github.com>
2024-02-21 14:17:42 +0100

    feat: nix package

--- flake.nix
+++ flake.nix
@@ -15,6 +15,29 @@
       system: let
         pkgs = import nixpkgs {inherit system;};
       in {
+        defaultPackage = pkgs.stdenv.mkDerivation {
+          name = "fscs-website-theme";
+          src = self;
+
+          buildInputs = with pkgs; [
+            hugo
+          ];
+
+          buildPhase = ''
+            mkdir assets
+            ln -s ${pkgs.fetchFromGitHub {
+              owner = "tabler";
+              repo = "tabler-icons";
+              rev = "v2.47.0";
+              hash = "sha256-7WawRKx15zbd0Gngcu+L6ztOQcKqLJvf98dg2jrKbzw=";
+            }} assets/icons
+          '';
+
+          installPhase = ''
+            cp -r . $out
+          '';
+        };
+
         devShell = pkgs.mkShell {
           nativeBuildInputs = with pkgs; [
             hugo
