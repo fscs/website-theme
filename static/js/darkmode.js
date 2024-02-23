5ef516b25378b4bb138ce6ad3db6a636a0eec24e
0x5a4 <54070204+0x5a4@users.noreply.github.com>
2024-02-23 12:52:10 +0100

    chore: clean up javascript

--- static/js/darkmode.js
+++ static/js/darkmode.js
@@ -1,27 +1,30 @@
+var enableDarkmode = false;
 
-init();
-function init() {
-    // check if the user has a theme cookie
-    if (document.cookie.includes("theme")) {
-        console.log("cookie found");
-        var element = document.getElementById("theme");
-        if (document.cookie.includes("theme=dark")) {
-            element.setAttribute("data-bs-theme", "dark");
-        } else {
-            element.setAttribute("data-bs-theme", "light");
-        }
-    }else{
-        if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
-            var element = document.getElementById("theme");
-            element.setAttribute("data-bs-theme", "dark");
-        }
+if (
+    window.matchMedia &&
+    window.matchMedia("(prefers-color-scheme: dark)").matches
+) {
+    enableDarkmode = true;
+}
+
+if (document.cookie.includes("theme")) {
+    if (document.cookie.includes("theme=dark")) {
+        enableDarkmode = true;
+    } else {
+        enableDarkmode = false;
     }
 }
 
+const element = document.getElementById("theme");
+if (enableDarkmode) {
+    element.setAttribute("data-bs-theme", "dark");
+} else {
+    element.setAttribute("data-bs-theme", "light");
+}
 
 function toggle_dark_mode() {
-    document.cookie = "theme=light";
     var element = document.getElementById("theme");
+
     if (element.getAttribute("data-bs-theme") == "light") {
         element.setAttribute("data-bs-theme", "dark");
         document.cookie = "theme=dark; path=/";
