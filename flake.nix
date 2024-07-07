b7702c36272bd830ccf42e047d61ca0a60327662
0x5a4 <54070204+0x5a4@users.noreply.github.com>
2024-07-08 01:02:33 +0200

    build: update flake

--- flake.nix
+++ flake.nix
@@ -7,6 +7,11 @@
       url = "github:numtide/flake-utils";
     };
 
+    icons = {
+      url = "github:tabler/tabler-icons/v3.1.0";
+      flake = false;
+    };
+    
     server = {
       type = "git";
       url = "ssh://git@github.com/fscs/website-server.git";
@@ -19,86 +24,91 @@
     self,
     nixpkgs,
     flake-utils,
+    icons,
     server,
   }:
     flake-utils.lib.eachDefaultSystem (
       system: let
         pkgs = import nixpkgs {inherit system;};
-
-        tabler-icons = pkgs.fetchFromGitHub {
-          owner = "tabler";
-          repo = "tabler-icons";
-          rev = "v3.1.0";
-          hash = "sha256-dHMXORjkIvoK1CgfdifRPl1iyDJ+t5WFyhCFk5QhZCY=";
-        };
       in rec {
-        packages.default = pkgs.stdenv.mkDerivation {
-          name = "fscs-website-theme";
-          src = self;
+        packages = rec {
+          default = pkgs.stdenv.mkDerivation {
+            name = "fscs-website-theme";
+            src = self;
 
-          buildInputs = with pkgs; [
-            hugo
-          ];
+            buildInputs = with pkgs; [
+              hugo
+            ];
 
-          buildPhase = ''
-            mkdir -p assets # p flag to not fail if the directory exists
-            ln -s ${tabler-icons}/icons assets/icons
-          '';
+            buildPhase = ''
+              mkdir -p assets # p flag to not fail if the directory exists
+              ln -s ${icons}/icons assets/icons
+            '';
 
-          installPhase = ''
-            cp -r . $out
+            installPhase = ''
+              cp -r . $out
+            '';
+          };
+        
+          demoSite = pkgs.stdenv.mkDerivation {
+            name = "demoSite";
+
+            src = ./demo;
+
+            buildInputs = with pkgs; [
+              hugo
+              pagefind
+            ];
+
+            buildPhase = ''
+              mkdir themes
+              ln -s ${default} themes/knut
+
+              hugo -e nix -d site
+              hugo -e nix-hidden -d site_hidden
+              hugo -e nix-private -d site_auth
+              
+              pagefind --site site
+              pagefind --site site_hidden
+              pagefind --site site_auth
+            '';
+
+            installPhase = ''
+                mkdir $out
+                cp -r site $out/static
+                cp -r site_hidden $out/static_hidden
+                cp -r site_auth $out/static_auth
+            '';
+          };
+
+          runDemoSite = pkgs.writeScriptBin "run.sh" ''
+              #!/usr/bin/env bash
+              DATA_DIR="$PWD/db/data"
+              SOCKET_DIR="$PWD/db/sockets"
+              SOCKET_URL="$(echo $SOCKET_DIR | sed 's/\//%2f/g')"
+              export DATABASE_URL="postgresql://$SOCKET_URL:5432/postgres"
+
+              mkdir -p "$DATA_DIR" "$SOCKET_DIR"
+
+              echo Initializing the Database
+              ${pkgs.postgresql}/bin/initdb -D "$DATA_DIR" --locale=C.utf8
+
+              echo Starting the Database
+              ${pkgs.postgresql}/bin/pg_ctl -D $DATA_DIR -o "-k $SOCKET_DIR -h \"\"" start
+
+              echo Starting the server
+              ${server.defaultPackage.${system}}/bin/fscs-website-backend \
+                  --host 0.0.0.0 \
+                  --port 8080 \
+                  --database-url $DATABASE_URL \
+                  --content-dir ${demoSite}/static \
+                  --private-content-dir ${default}/static_auth \
+                  --hidden-content-dir ${default}/static_hidden
+
+              echo Stopping the Database
+              ${pkgs.postgresql}/bin/pg_ctl -D "$DATA_DIR" stop
           '';
         };
-
-        packages.demoSite = pkgs.stdenv.mkDerivation {
-          name = "demoSite";
-
-          src = ./demo;
-
-          buildInputs = with pkgs; [
-            hugo
-            go
-            git
-            pagefind
-          ];
-
-          buildPhase = ''
-            mkdir themes
-            ln -s ${packages.default} themes/knut
-
-            hugo -e nix
-            pagefind --site public
-          '';
-
-          installPhase = ''
-            mkdir -p $out/bin
-            cp -r public $out/bin/static
-            cp -r ${server.defaultPackage.${system}}/bin/migrations $out/bin/migrations
-            cp ${server.defaultPackage.${system}}/bin/fscs-website-backend $out/bin/fscs-website-backend
-          '';
-        };
-
-        packages.runDemoSite = pkgs.writeScriptBin "run.sh" ''
-          #!/usr/bin/env bash
-          DATA_DIR="$PWD/db/data"
-          SOCKET_DIR="$PWD/db/sockets"
-          SOCKET_URL="$(echo $SOCKET_DIR | sed 's/\//%2f/g')"
-          export DATABASE_URL="postgresql://$SOCKET_URL:5432/postgres"
-
-          mkdir -p "$DATA_DIR" "$SOCKET_DIR"
-
-          echo Initializing the Database
-          ${pkgs.postgresql}/bin/initdb -D "$DATA_DIR" --locale=C.utf8
-
-          echo Starting the Database
-          ${pkgs.postgresql}/bin/pg_ctl -D $DATA_DIR -o "-k $SOCKET_DIR -h \"\"" start
-
-          echo Starting the server
-          ${packages.demoSite}/bin/fscs-website-backend --database-url $DATABASE_URL --use-executable-dir
-
-          echo Stopping the Database
-          ${pkgs.postgresql}/bin/pg_ctl -D "$DATA_DIR" stop
-        '';
 
         apps.default = flake-utils.lib.mkApp {
           drv = packages.runDemoSite;
